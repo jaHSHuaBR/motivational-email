524b0ee4fbf59dbcb7831d657655794de33a1015
William J. Bruton <96779322+jaHSHuaBR@users.noreply.github.com>
2022-03-09 17:16:08 -0500

    Update main.py
    Fixed program by removing my personal email and password, and plugging in constants for that info that anybody could easily fill in to run properly

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import datetime as dt
 from smtplib import *
 import random
 
-# Enter your login info here
-my_email = "YOUR EMAIL HERE"    
-password = "YOUR PASSWORD HERE"
+# ENTER your login info here, recipient email, and smtp server for YOUR SENDING email:
+MY_EMAIL = "YOUR EMAIL HERE"    
+MY_PASSWORD = "YOUR EMAIL'S PASSWORD HERE"
+RECIPIENT_EMAIL = "EMAIL YOU WISH TO SEND TO"
+# Yahoo: smtp.mail.yahoo.com, Gmail: smtp.gmail.com  - If using other email provider, just search google for "smtp server"
+EMAIL_SMTP = "smtp.mail.yahoo.com"
+
 
 now = dt.datetime.now()
 day_of_week = now.weekday()
@@ -16,15 +20,14 @@ if day_of_week == 2:
         quote = random.choice(data)
 
     # Opening connection - Replace "smtp.mail.yahoo.com" with your emails smtp address. Gmail: smtp.gmail.com 
-    with SMTP("smtp.mail.yahoo.com") as connection:
+    with SMTP(EMAIL_SMTP) as connection:
         # Securing Connection
         connection.starttls()
         # Login in to email
-        connection.login(user=my_email, password=password)
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
         connection.sendmail(
-            # INSERT YOUR "FROM EMAIL" AND "TO EMAIL" HERE:
-            from_addr=my_email,
-            to_addrs="jahshuabr@gmail.com",
+            from_addr=MY_EMAIL,
+            to_addrs=RECIPIENT_EMAIL,
             # The content of email (Here is the random quote)
             msg=f"Subject:Motivational Quote\n\n{quote}"
         )

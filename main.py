0fde6784b40444af9091cf39f144008fd253ab64
William J. Bruton <96779322+jaHSHuaBR@users.noreply.github.com>
2022-03-10 23:56:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,18 +8,19 @@ MY_PASSWORD = "YOUR EMAIL'S PASSWORD HERE"
 RECIPIENT_EMAIL = "EMAIL YOU WISH TO SEND TO"
 # Yahoo: smtp.mail.yahoo.com, Gmail: smtp.gmail.com  - If using other email provider, just search google for "smtp server"
 EMAIL_SMTP = "smtp.mail.yahoo.com"
-
+# Change to day you want email to send. Monday=0, Tuesday=1...Sunday=6
+DAY_TO_SEND = 0
 
 now = dt.datetime.now()
 day_of_week = now.weekday()
 
-if day_of_week == 2:
+if day_of_week == DAY_TO_SEND:
 
     with open("quotes.txt", "r") as quotes:
         data = quotes.readlines()
         quote = random.choice(data)
 
-    # Opening connection - Replace "smtp.mail.yahoo.com" with your emails smtp address. Gmail: smtp.gmail.com 
+    # Opening connection 
     with SMTP(EMAIL_SMTP) as connection:
         # Securing Connection
         connection.starttls()
